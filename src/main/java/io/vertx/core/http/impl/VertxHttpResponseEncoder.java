135dc079bfab27c053aa89e7e0212b14aee25113
franz1981 <nigro.fra@gmail.com>
2022-08-11 17:19:56 +0200

    Optimized full http response type

--- src/main/java/io/vertx/core/http/impl/VertxHttpResponseEncoder.java
+++ src/main/java/io/vertx/core/http/impl/VertxHttpResponseEncoder.java
@@ -19,12 +19,26 @@ import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpResponseEncoder;
 import io.vertx.core.http.impl.headers.HeadersMultiMap;
 
+import java.util.List;
+
 /**
  * {@link io.netty.handler.codec.http.HttpResponseEncoder} which forces the usage of direct buffers for max performance.
  *
  * @author <a href="mailto:nmaurer@redhat.com">Norman Maurer</a>
  */
 final class VertxHttpResponseEncoder extends HttpResponseEncoder {
+
+  public VertxHttpResponseEncoder() {
+    super(AssembledFullHttpResponse.class);
+  }
+
+  @Override
+  public boolean acceptOutboundMessage(Object msg) throws Exception {
+    return msg instanceof AssembledFullHttpResponse ||
+      msg instanceof AssembledHttpResponse ||
+      msg instanceof AssembledLastHttpContent ||
+      super.acceptOutboundMessage(msg);
+  }
 
   @Override
   protected void encodeHeaders(HttpHeaders headers, ByteBuf buf) {
